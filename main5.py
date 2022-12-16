31970749ffe72fbd39a3997c510f4105ed81a550
rimsal <rima.salnaitiene@gmail.com>
2022-12-16 09:52:25 +0200

    naujas

--- main5.py
+++ main5.py
@@ -1,1 +1,66 @@
-django programa
+# django programa
+
+import pickle
+
+# a = 1024
+#
+# with open("a.pkl", "wb") as pickle_out:
+#     pickle.dump(a, pickle_out)
+#
+# with open("a.pkl", "rb") as pickle_in:
+#     naujas_a = pickle.load(pickle_in)
+#
+# print(naujas_a)
+#
+# zodynas = {1:"Pirmas", 2:"Antras", 3:"Trečias"}
+#
+# with open("zodynas.pkl", "wb") as pickle_out:
+#     pickle.dump(zodynas, pickle_out)
+#
+# with open("zodynas.pkl", "rb") as pickle_in:
+#     naujas_zodynas = pickle.load(pickle_in)
+#
+# print(naujas_zodynas)
+#
+# a = 10
+# b = 7
+# c = 23
+#
+# with open("abc.pkl", "wb") as pickle_out:
+#     pickle.dump(a, pickle_out)
+#     pickle.dump(b, pickle_out)
+#     pickle.dump(c, pickle_out)
+#
+# with open("abc.pkl", "rb") as pickle_in:
+#     nauja_a = pickle.load(pickle_in)
+#     nauja_b = pickle.load(pickle_in)
+#     nauja_c = pickle.load(pickle_in)
+#
+# print(nauja_a)
+# print(nauja_b)
+# print(nauja_c)
+#
+# with open("abc.pkl", "rb") as pickle_in:
+#     while True:
+#         try:
+#             print(pickle.load(pickle_in))
+#         except EOFError:
+#             break
+
+class Automobilis:
+    def __init__(self, marke, modelis):
+        self.marke = marke
+        self.modelis = modelis
+
+automobiliai = [Automobilis("Toyota", "Avensis"), Automobilis("Toyota", "Corolla"), Automobilis("Toyota", "Camry")]
+
+with open("automobilis.pkl", "wb") as failas:
+    pickle.dump(automobiliai, failas)
+
+with open("automobilis.pkl", "rb") as failas:
+    automobiliai = pickle.load(failas)
+    for automobilis in automobiliai:
+        print("Markė", automobilis.marke)
+        print("Modelis", automobilis.modelis)
+
+
--- main6.py
+++ main6.py
@@ -1,26 +1,26 @@
 from _datetime import datetime
-
-zen = '''The Zen of Python, by Tim Peters
-
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!'''
+#
+# zen = '''The Zen of Python, by Tim Peters
+#
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!'''
 
 
 # 1. Sukurti failą tekstas.txt su tekstu:
@@ -92,8 +92,6 @@ Namespaces are one honking great idea -- let's do more of those!'''
 # BIUDZETO SUDARYMAS
 
 import pickle
-
-   import pickle
 def nuskaityti_is_failo():
     try:
         with open("didzioji_knyga.pkl", 'rb') as file:
