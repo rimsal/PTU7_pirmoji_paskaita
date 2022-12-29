4a3a1e2666f3382fb535d11db72b0c4df99a11c2
rimsal <rima.salnaitiene@gmail.com>
2022-12-29 14:22:53 +0200

    projektas Tankas taisytas

--- tankas1223_0106/tankas.py
+++ tankas1223_0106/tankas.py
@@ -1,61 +1,99 @@
-import random, math
+import random
 
-
-class Tankas:
-
+class Tank:
     def __init__(self):
         self.x = 0
         self.y = 0
-        self.zingsnis = 'pirmyn'
-        self.suvio_kryptis = {'pirmyn': 0, 'atgal': 0, 'kairen': 0, 'desinen': 0}
-        self.suviu_skaicius = 0
+        self.direction = 'pirmyn'
+        self.shot_directions = {'pirmyn': 0, 'atgal': 0, 'kairen': 0, 'desinen': 0}
+        self.targets_hit = 0
 
-    def judeti(self, pajudeti):
-        self.zingsnis = pajudeti
-        if pajudeti == 'pirmyn':
+    def move(self, direction_to_move):
+        self.direction = direction_to_move
+        if direction_to_move == 'pirmyn':
             self.y += 1  # Tankas pavažiavo į priekį
-        elif pajudeti == 'atgal':
+        elif direction_to_move == 'atgal':
             self.y -= 1  # Tankas pavažiavo į atgal
-        elif pajudeti == 'kairen':
+        elif direction_to_move == 'kairen':
             self.x -= 1  # Tankas pavažiavo į kairę
-        elif pajudeti == 'desinen':
+        elif direction_to_move == 'desinen':
             self.x += 1  # Tankas pavažiavo į dešinę
 
-    def sauti(self, taskasx, taskasy):
-       self.x = taskasx
-       self.y = taskasy
+    def shoot(self, targetx, targety):
 
-
-        # print('Šūvis!')
-        self.suvio_kryptis[self.zingsnis] += 1
-        if self.x == taskasx:
-            if self.y < taskasy:
-                if self.zingsnis == 'pirmyn':
-                    self.suviu_skaicius += 1
+        print('Šūvis!')
+        self.shot_directions[self.direction] += 1
+        if self.x == targetx:
+            if self.y < targety:
+                if self.direction == 'pirmyn':
+                    self.targets_hit += 1
                     return True
             else:
-                if self.zingsnis == 'atgal':
-                    self.suviu_skaicius += 1
+                if self.direction == 'atgal':
+                    self.targets_hit += 1
                     return True
-        if self.y == taskasy:
-            if self.x < taskasx:
-                if self.zingsnis == 'desinen':
-                    self.suviu_skaicius += 1
+        if self.y == targety:
+            if self.x < targetx:
+                if self.direction == 'desinen':
+                    self.targets_hit += 1
                     return True
             else:
-                if self.zingsnis == 'kairen':
-                    self.suviu_skaicius += 1
+                if self.direction == 'kairen':
+                    self.targets_hit += 1
                     return True
 
         return False
 
-
-
-def info(self):
-    pass
-
-        print(f"Kryptis: {self.zingsnis}")
+    def info(self):
+        print(f"Kryptis: {self.direction}")
         print(f"Koordinatės: {self.x}:{self.y}")
-        print(f"Šūvių iš viso: {sum(self.suvio_kryptis.values())}")
-        print(f"{self.suvio_kryptis}")
-        print((f"Taškai: {self.suviu_skaicius}"))
+        print(f"Šūvių iš viso: {sum(self.shot_directions.values())}")
+        print(f"{self.shot_directions}")
+        print((f"Targets Hit: {self.targets_hit}"))
+
+
+class Target:
+    def __init__(self):
+        self.x = random.randint(-5, 5)
+        self.y = random.randint(-5, 5)
+
+    def reset(self):
+        self.x = random.randint(-5, 5)
+        self.y = random.randint(-5, 5)
+
+
+tankas = Tank()
+taikinys = Target()
+POINTS = 100
+
+print("Sveiki, žaidžiam!")
+while POINTS != 0:  # Programa veiks kol taškai nesibaigs.
+    print("Judesiai:")
+    print("w: aukštyn, s: žemyn, a: kairėn, d: dešinėn")
+    print("b: Šūvis, i: informacija, q: išeiti")
+    print(f"x: {tankas.x}, y: {tankas.y}")
+    x = str(input("Tavo pasirinkimas: "))
+    POINTS -= 10  # už kiekvieną ėjimą nuskaitomi 10 taškų
+
+    if x == 'w':
+        tankas.move('pirmyn')
+    elif x == 's':
+        tankas.move('atgal')
+    elif x == 'a':
+        tankas.move('kairen')
+    elif x == 'd':
+        tankas.move('desinen')
+    elif x == 'b':
+        if tankas.shoot(taikinys.x, taikinys.y):
+            print('Pataikei')
+            POINTS += 50  # Pataikius pridedami 50 taškų
+            taikinys.reset()  # Per naują nustatomas taikinys
+    elif x == 'i':
+        tankas.info()
+    elif x == "q":
+        print(f"Targets Hit: {tankas.targets_hit}")
+        break
+    elif x == "c":
+        print(f"{taikinys.x}: {taikinys.y}")
+
+print("Taškai baigėsi.\nGalite pradėti iš naujo.")
